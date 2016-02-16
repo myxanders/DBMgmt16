cfcb8ac8bcbd31c8742d80ccd605e61abd5ee476
myxanders <Mitchell.Xanders1@marist.edu>
2016-02-16 09:25:41 -0500

    Lab #4 as a .sql file

---
+++ Lab4.sql
@@ -0,0 +1,84 @@
+--Mitchell Xanders 2/16/16 Lab 4
+
+--Query 1
+select city
+from agents
+where aid in (select aid
+	      from orders
+	      where cid = 'c002'
+	      );
+
+--Query 2
+select pid
+from orders
+where aid in (select aid
+	      from orders
+	      where cid in (select cid
+			    from customers
+			    where city = 'Dallas'
+			    )
+	     )
+order by pid DESC;
+
+--Query 3
+select cid, name
+from customers
+where cid not in (select cid
+		  from orders
+		  where aid = 'a01'
+		 );
+
+--Query 4
+select cid, name
+from customers
+where cid in (select cid
+	      from orders
+	      where pid = 'p01' and cid in (select cid
+					    from orders
+					    where pid = 'p07'
+					    )
+	      ); 
+
+--Query 5
+select distinct pid 
+from orders
+where cid not in (select cid
+		  from orders
+		  where aid = 'a07'
+		  )
+order by pid DESC;
+
+--Query 6
+select name, discount, city
+from customers
+where cid in (select cid
+	      from orders
+	      where aid in (select aid
+			    from agents
+			    where city in ('London', 'New York')
+			    )
+	      );
+
+--Query 7
+select *
+from customers
+where cid not in (select cid
+		  from customers
+		  where city in ('Dallas', 'London')
+		  )
+and discount in (select discount
+		 from customers
+		 where city in ('Dallas', 'London')
+		 );
+
+--Question #8
+-- Check constraints are put in place to maintain table integrity when we insert, delete or update data.
+-- These constraints allow us to establish primary keys, default data types, and not null values, for example.
+-- An example of a good check constraint is using the NOT NULL constraint involving information like a Student ID.
+-- In a database storing student information, it is of good practice to give each student a unique ID number and set is as the primary key.
+-- With the NOT NULL constraint on it, the Student ID value must not be left empty for any student in order to insert their information into the database.
+-- Bad practice of the check constraint NOT NULL would be using it in a column for "Middle Name".
+-- Not everybody has a middle name, so setting "Middle Name" to NOT NULL would prevent some students from having their information kept in the database.
+-- Because every student must be issued a Student ID number, it makes sense that the Student ID Number must be included in their information in the datatbase.
+-- However, because not every students has a middle name, it does not make sense to require the input of a middle name in order to include a studnet in the database.
+-- 
