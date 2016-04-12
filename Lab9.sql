500d9e98fac85f21cb14955a998ef5e4f8297220
myxanders <Mitchell.Xanders1@marist.edu>
2016-04-12 10:11:53 -0400

    Normalization 3 sql

---
+++ Lab9.sql
@@ -0,0 +1,12 @@
+2. 
+People: pid -> firstName, lastName, birthDate, aid, phoneNum
+Coaches: cid -> exp
+Head Coaches: hcid -> 
+Assistant Coaches: acid ->
+Teams: tid, lid -> teamName, spotsRem, primColor, secColor
+Leagues: lid -> ageGroup, numTeams
+Players: kid, tid -> position, bat, throw
+Address: aid -> adline1, adline2, city, state, zip
+Rosters: tid, pid, lid ->
+
+3. This database is in third normal form because all of the tables consist of independent, unstructrue values, do not consist of partial key dependencies, and the non-prime attributes are dependent on the whole keys of the table.
